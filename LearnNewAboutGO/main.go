c29c20885024468cba6111173cf9860bd93f548b
Alexand_Gavrin <nuke.dust@yandex.ru>
2024-08-12 00:26:22 +0300

    Something

---
+++ LearnNewAboutGO/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+	"strings"
+)
+
+var (
+	module uint64     = 10000007
+	maxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Asin(-5 + 12i)
+) // как объявлять глобальные переменные
+
+const (
+	tmpRune         rune = '▒'
+	someInteresting int  = 228
+) // Как объявлять глобальные константы
+
+type longLONG uint64    // именованный тип данных (удобно когда функция должна принимать только этот тип данных)
+type longerNum = uint64 // псевдоним
+
+func ModifuSpaces(s, mode string) string {
+	var newString string
+	switch mode {
+	case "dash":
+		newString = strings.ReplaceAll(s, " ", "-")
+		break
+	case "underscore":
+		newString = strings.ReplaceAll(s, " ", "_")
+	default:
+		newString = strings.ReplaceAll(s, " ", "*")
+	}
+	return newString
+}
+
+func main() {
+	fmt.Printf("Type: %T Value: %v\n", module, module)
+	fmt.Printf("Type: %T Value: %v\n", maxInt, maxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+
+}
--- TrySomeTasks/main.go
+++ TrySomeTasks/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("POSOSI")
+	dict := make(map[int]bool)
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	numbers := strings.Split(input, " ")
+	for _, num := range numbers {
+		tmp, _ := strconv.Atoi(num)
+		dict[tmp] = true
+	}
+	fmt.Println(len(dict))
 }
